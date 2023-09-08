1bfad2915cd1bc93d0bea6499aafdae35bbd3f69
Maksat <maksat_orunkhanov@kwaaka.com>
2023-09-09 03:17:33 +0600

    v5

--- service/greeting.go
+++ service/greeting.go
@@ -3,9 +3,9 @@ package service
 import "fmt"
 
 func SayHello() {
-	fmt.Println("hello from servicethree 4")
+	fmt.Println("hello from servicethree 5")
 }
 
 func SayGoodbye() {
-	fmt.Println("goodbye from servicethree 4")
+	fmt.Println("goodbye from servicethree 5")
 }
